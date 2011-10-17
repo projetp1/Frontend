f9ba3f356f3353d9c59ce68a74ce0270c4377eb9
Diego999 <diego.antognini@he-arc.ch>
2011-10-17 15:55:29 +0200

    Test de compilation

--- projetp1/src/com/github/projetp1/Main.java
+++ projetp1/src/com/github/projetp1/Main.java
@@ -5,8 +5,9 @@ public class Main {
 	/**
 	 * @param args
 	 */
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public static void main(String[] args) 
+	{
+		System.out.print("Teste !");
 
 	}
 
