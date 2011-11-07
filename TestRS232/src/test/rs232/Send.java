2f5b68874e98534cce9bd12597957d3a07c64bea
Sébastien Vaucher <seb.vaucher@gmail.com>
2011-11-07 15:20:57 +0100

    Envoi chaque seconde

--- TestRS232/src/test/rs232/Send.java
+++ TestRS232/src/test/rs232/Send.java
@@ -11,17 +11,24 @@ public class Send {
 		// TODO Auto-generated method stub
 		SerialPort serialPort = new SerialPort("COM4");
         try {
-            serialPort.openPort();//Open serial port
-            serialPort.setParams(SerialPort.BAUDRATE_9600, 
-                                 SerialPort.DATABITS_8,
-                                 SerialPort.STOPBITS_1,
-                                 SerialPort.PARITY_NONE);//Set params. Also you can set params by this string: serialPort.setParams(9600, 8, 1, 0);
-            serialPort.writeBytes("Dafdsfrewqgreagfagf !".getBytes());//Write data to port
-            serialPort.closePort();//Close serial port
+        	serialPort.openPort();//Open serial port
+        		for(int num =0;;num++) {
+	            serialPort.setParams(SerialPort.BAUDRATE_9600, 
+	                                 SerialPort.DATABITS_8,
+	                                 SerialPort.STOPBITS_1,
+	                                 SerialPort.PARITY_NONE);//Set params. Also you can set params by this string: serialPort.setParams(9600, 8, 1, 0);
+	            
+	            serialPort.writeBytes(String.valueOf(num).getBytes());//Write data to port
+	            System.out.println("Sent num "+String.valueOf(num));
+	            Thread.sleep(1000);
+        		}
         }
         catch (SerialPortException ex) {
             System.out.println(ex);
-        }
+        } catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 
 }
