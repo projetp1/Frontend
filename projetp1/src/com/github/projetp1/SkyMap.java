326097fe75b71a493e171bce4a9df555584ebde1
Alexandre Perez <alexandre.perez92@gmail.com>
2012-02-14 13:32:28 +0100

    Ajout de variables et du constructeur
    Signed-off-by: Alexandre Perez <alexandre.perez92@gmail.com>

--- projetp1/src/com/github/projetp1/SkyMap.java
+++ projetp1/src/com/github/projetp1/SkyMap.java
@@ -14,23 +14,53 @@ public class SkyMap {
 	private int zoom;
 	private double longitude;
 	private double latitude;
+	private ArrayList<CelestialObject> arrayObject;
 	
 	/**
 	 * 
 	 */
-	public SkyMap() {
+	public SkyMap(int _zoom) {
 		// TODO Auto-generated constructor stub
+		this.setZoom(_zoom);
 	}
 
-	public void updateSkyMap(int _zoom, double _longitude, double _latitude) {
+	public void updateSkyMap() {
 		
 	}
 	
 	public void updateArrow(CelestialObject _object) {
 		
 	}
-	
-	public void showCelestialObjects(ArrayList<CelestialObject> _arrayObject) {
-		
+
+	public int getZoom() {
+		return zoom;
+	}
+
+	public void setZoom(int _zoom) {
+		this.zoom = _zoom;
+	}
+
+	public double getLatitude() {
+		return latitude;
+	}
+
+	public void setLatitude(double _latitude) {
+		this.latitude = _latitude;
+	}
+
+	public double getLongitude() {
+		return longitude;
+	}
+
+	public void setLongitude(double _longitude) {
+		this.longitude = _longitude;
+	}
+
+	public ArrayList<CelestialObject> getArrayObject() {
+		return arrayObject;
+	}
+
+	public void setArrayObject(ArrayList<CelestialObject> _arrayObject) {
+		this.arrayObject = _arrayObject;
 	}
 }
