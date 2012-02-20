304cadf441142d09d41228f968b74d58dc8932c5
Sébastien Vaucher <seb.vaucher@gmail.com>
2012-02-20 16:07:42 +0100

    Début initialisation

--- projetp1/src/com/github/projetp1/RS232.java
+++ projetp1/src/com/github/projetp1/RS232.java
@@ -3,19 +3,32 @@
  */
 package com.github.projetp1;
 
+import jssc.*;
+
 /**
  * @author alexandr.perez
- *
+ * @author sebastie.vaucher
  */
 public class RS232 {
 
 	private String port;
+	protected Settings _settings;
+	private SerialPort _sp;
 	
 	/**
 	 * 
 	 */
 	public RS232() {
-		// TODO Auto-generated constructor stub
+		// Initialisation des valeurs par défaut :
+		this.port = _settings.getPort();
+		if(this.port == null) {
+			
+			this.port = (SerialPortList.getPortNames())[0];
+			this._settings.setPort(this.port);
+		}
+		else {
+			this.port = this._settings.getPort();
+		}
 	}
 	
 	public Boolean connect(String _port) {
