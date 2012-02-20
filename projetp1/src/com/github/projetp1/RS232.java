f9ca1f5aaed69da66222fa4293fc1923c1aa014a
Loris Ceschin <ceschinlori@gmail.com>
2012-02-20 16:45:28 +0100

    Initialisation du rs232 v1

--- projetp1/src/com/github/projetp1/RS232.java
+++ projetp1/src/com/github/projetp1/RS232.java
@@ -18,17 +18,35 @@ public class RS232 implements SerialPortEventListener {
 	/**
 	 * 
 	 */
-	public RS232() {
+	public RS232() 
+	{
 		// Initialisation des valeurs par défaut :
+		
+		// Initialisation du port:
 		this.port = _settings.getPort();
-		if(this.port == null) {
-			
+		if(this.port == null) 
+		{
 			this.port = (SerialPortList.getPortNames())[0];
 			this._settings.setPort(this.port);
 		}
-		else {
-			this.port = this._settings.getPort();
+		
+		// Initialisation des settings
+		//TODO: this._settings = new ();
+		
+		// Initialisation du sp
+		this._sp = new SerialPort(this.port);
+		
+		try 
+		{
+			this._sp.openPort();//Open serial port
+			this._sp.setParams(SerialPort.BAUDRATE_9600, 
+									SerialPort.DATABITS_8,
+									SerialPort.STOPBITS_1,
+									SerialPort.PARITY_NONE);//Set params. Also you can set params by this string: serialPort.setParams(9600, 8, 1, 0);
 		}
+        catch (Exception ex) {
+            System.out.println(ex);
+        }
 	}
 	
 	public Boolean connect(String _port) {
