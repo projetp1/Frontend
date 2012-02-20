0a78ebf971022d67f6399990ffbf151a953bb3dd
Sébastien Vaucher <seb.vaucher@gmail.com>
2012-02-20 17:15:19 +0100

    serialEvent (non terminé)
    Signed-off-by: Sébastien Vaucher <seb.vaucher@gmail.com>

--- projetp1/src/com/github/projetp1/RS232.java
+++ projetp1/src/com/github/projetp1/RS232.java
@@ -63,9 +63,33 @@ public class RS232 implements SerialPortEventListener {
 		
 	}
 
+	
+	//TODO: Utiliser un buffer car jssc envoie les données sans les séparer.
 	@Override
 	public void serialEvent(SerialPortEvent e) {
 		// Callback du SerialPort
-		
+		if(!e.isRXCHAR()) //If no data is available
+			return;
+		        
+		// TODO: Try/Catch
+        String received;
+		try {
+			received = _sp.readString();
+		} catch (SerialPortException ex) {
+			// TODO Auto-generated catch block
+			ex.printStackTrace();
+			return;
+		}
+        if(received.toUpperCase().startsWith("CTRL")) { //Trame de contrôle
+        	String[]datas = received.substring(5).split(";");
+        }
+        else if (received.toUpperCase().startsWith("DATA")) { //Trame de donnée
+        	
+        }
+        else //Trame invalide
+        {
+        	System.err.println("Trame invalide reçue");
+        	return;
+        }
 	}
 }
