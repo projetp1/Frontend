a7c7d8b7dc6437854290b1f6b1901da0d6cbbe15
Diego999 <diego.antognini@he-arc.ch>
2012-02-25 17:51:57 +0100

    Mise à jour du fichier pour les tests

--- projetp1/src/com/github/projetp1/Test.java
+++ projetp1/src/com/github/projetp1/Test.java
@@ -10,16 +10,20 @@ public class Test {
 		    DataBase db = new DataBase(sDataBase);
 		    
 		    //CelestialObject test = new CelestialObject(1,0,-1,-1,-1,-1,-1,"Sol",0,0,0.000004848,0,0,0,-26.73,4.85,"G2V",0.656,0,0,0,0,0,0);//-1 = null car dans ces champs, cette valeur est impossible !
-		    //test.getHeader();
-		    //test.getInfos();
 		    
 		    ArrayList<CelestialObject> stars = new ArrayList<CelestialObject>();
 		    stars = db.starsForCoordinates(1.0, -2.0); 
 
-		    stars.get(0).getHeader();
-		    
-		    for(int i=0;i<stars.size();i++)
-		    	stars.get(i).getInfos();
+		    if(stars.size() != 0)
+		    {
+			    stars.get(0).getHeader();
+			    
+			    for(int i=0;i<stars.size();i++)
+			    	stars.get(i).getInfos();
+		    }
+		    else
+		    	System.out.println("Aucun résultat n'a été trouvé dans la base de données");
+
 		    
 		    db.closeConnection();
 		  }
