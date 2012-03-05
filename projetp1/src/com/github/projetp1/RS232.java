a5dec97b16995713b35b87104f8608a8721c2ce4
Sébastien Vaucher <seb.vaucher@gmail.com>
2012-03-05 15:06:57 +0100

    RS-232 (Peut-être obsolète, car le protocole n'est pas encore défini)

--- projetp1/src/com/github/projetp1/RS232.java
+++ projetp1/src/com/github/projetp1/RS232.java
@@ -3,28 +3,38 @@
  */
 package com.github.projetp1;
 
+import java.util.concurrent.ConcurrentLinkedQueue;
+
+import com.sun.xml.internal.bind.v2.util.FatalAdapter;
+
 import jssc.*;
 
 /**
  * @author alexandr.perez
  * @author sebastie.vaucher
  */
-public class RS232 implements SerialPortEventListener {
+public class RS232 implements SerialPortEventListener
+{
 
 	private String port;
 	protected Settings _settings;
 	private SerialPort _sp;
+	private ConcurrentLinkedQueue<Object[]> _dataQueue = new ConcurrentLinkedQueue<>();
+	private ConcurrentLinkedQueue<String> _ctrlQueue = new ConcurrentLinkedQueue<>();
+	private String buffer = "";
+	
+	private final char EOT = 4;
 	
 	/**
 	 * 
 	 */
-	public RS232() 
+	public RS232()
 	{
 		// Initialisation des valeurs par défaut :
 		
 		// Initialisation du port:
 		this.port = _settings.getPort();
-		if(this.port == null) 
+		if(this.port == null)
 		{
 			this.port = (SerialPortList.getPortNames())[0];
 			this._settings.setPort(this.port);
@@ -49,24 +59,32 @@ public class RS232 implements SerialPortEventListener {
         }
 	}
 	
-	public Boolean connect(String _port) {
+	public Boolean connect(String _port)
+	{
 		port = _port;
 		return true;
 	}
 	
-	public Boolean disconnect() {
+	public Boolean disconnect()
+	{
 		
 		return true;
 	}
 	
-	public void sendData(String data) {
-		
+	private void sendAck(int frame) throws SerialPortException
+	{
+		_sp.writeString("CTRL:ACK"+String.valueOf(frame));
 	}
-
+	
+	private void sendNck(int frame) throws SerialPortException
+	{
+		_sp.writeString("CTRL:NCK"+String.valueOf(frame));
+	}
 	
 	//TODO: Utiliser un buffer car jssc envoie les données sans les séparer.
 	@Override
-	public void serialEvent(SerialPortEvent e) {
+	public void serialEvent(SerialPortEvent e)
+	{
 		// Callback du SerialPort
 		if(!e.isRXCHAR()) //If no data is available
 			return;
@@ -80,8 +98,19 @@ public class RS232 implements SerialPortEventListener {
 			ex.printStackTrace();
 			return;
 		}
+		
+		if(received.indexOf(EOT) == -1)
+		{
+			buffer += received;
+			return;
+		}
+		
         if(received.toUpperCase().startsWith("CTRL")) { //Trame de contrôle
         	String[]datas = received.substring(5).split(";");
+        	for (String str : datas)
+        		_ctrlQueue.add(str);
+        	
+        	
         }
         else if (received.toUpperCase().startsWith("DATA")) { //Trame de donnée
         	
@@ -93,3 +122,4 @@ public class RS232 implements SerialPortEventListener {
         }
 	}
 }
+
