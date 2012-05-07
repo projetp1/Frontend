9b534a0fc0e4bccedfc885ecaef3a99d0b3b6271
Diego999 <diego.antognini@he-arc.ch>
2012-05-07 16:49:46 +0200

    Suppression méthode getHeader

--- projetp1/src/com/github/projetp1/CelestialObject.java
+++ projetp1/src/com/github/projetp1/CelestialObject.java
@@ -109,14 +109,14 @@ public class CelestialObject {
 		//this.dVXYZ[2] = _vz;	
 	}
 
-	/**
-	 * getHeader
-	 * Gives the header of columns from the database
-	 */
-	public void getHeader()
-	{
-		System.out.println("ID\tStarID\tHIP\tHD\tHR\tGliese\tBayerFlamsteed\tProperName\tRA\tDec\tDistance\tPMRA\tPMDec\tRV\tMag\tAbsMag\tSpectrum\tColorIndex\tX\tY\tZ\tVX\tVY\tVZ\tXReal\tYReal");
-	}
+	///**
+	// * getHeader
+	// * Gives the header of columns from the database
+	// */
+	//public void getHeader()
+	//{
+	//	System.out.println("ID\tStarID\tHIP\tHD\tHR\tGliese\tBayerFlamsteed\tProperName\tRA\tDec\tDistance\tPMRA\tPMDec\tRV\tMag\tAbsMag\tSpectrum\tColorIndex\tX\tY\tZ\tVX\tVY\tVZ\tXReal\tYReal");
+	//}
 	
 	/*
 	 * getInfos
