efe522f4c7b24a8645fedd333c843d5eabc336fd
Issa Barbier <issa.barbier@gmail.com>
2012-05-21 14:52:17 +0200

    correction d'un bug d'affichage de la boussole. avancement de la fenaitre d'aide et de la bare de recherche (90%)
    Signed-off-by: Issa Barbier <issa.barbier@gmail.com>

---
+++ projetp1/src/com/github/projetp1/ListModel.java
@@ -0,0 +1,54 @@
+package com.github.projetp1;
+
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Set;
+import javax.swing.AbstractListModel;
+
+
+public class ListModel<F> extends AbstractListModel {
+
+    ArrayList<F> list = new ArrayList<F>();
+
+    public ListModel() {}
+
+    public int getSize() {
+        return list.size();
+    }
+
+    public Object getElementAt(int index) {
+        return list.get(index);
+    }
+
+    public void setElement(F element) {
+        list.add(element);
+        super.fireIntervalAdded(this, 0, list.size() - 1);
+    }
+
+    public void removeElement(F element){
+        int index = list.indexOf(element);
+        list.remove(element);
+        super.fireIntervalRemoved(this, index, index);
+    }
+
+    public void removeElement(int index){
+        list.remove(index);
+//        super.fireIntervalAdded(this, 0, list.size() - 1);
+    }
+
+    public void removeAll() {
+        list.clear();
+    }
+
+    public void setNewList(Set<String> keySet) {
+        list.clear();
+        list.addAll((Collection<? extends F>) keySet);
+        super.fireIntervalAdded(this, 0, list.size() - 1);
+    }
+
+    public String [] toArray() {
+        return list.toArray(new String [] {});
+    }
+
+}
