fc1d3c87022f110dee6114f734d52d9943aa58ee
Sébastien Vaucher <seb.vaucher@gmail.com>
2012-08-29 10:54:11 +0200

    Show the coordinates in DMS format

--- projetp1/src/com/github/projetp1/MainView.java
+++ projetp1/src/com/github/projetp1/MainView.java
@@ -181,27 +181,91 @@ public class MainView extends JFrame implements KeyListener
 
 
 	/**
-	 * This methode update the values form the pic.
+	 * This method updates the values form the PIC and sets the needles accordingly.
 	 */
-	
 	private void update() {
-		if (pic != null)
+		if(pic != null)
 		{
 			compassPanel.setGreenNeedle(pic.getAzimuth());
 			inclinometerPanel.setGreenNeedle(pic.getPitch());
-	
-			char hemNS = 'N', hemWE = 'E';
-			double lat = pic.getLatitude(), lon = pic.getLongitude();
-	
-			if (lat < 0.0)
-				hemNS = 'S';
-			if (lon < 0.0)
-				hemWE = 'W';
-			
-			coordinate.setText(Math.abs(lat) + "° " + hemNS + ", " + Math.abs(lon) + "° " + hemWE);
 		}
 
-		skymap.updateSkyMap();		
+		char hemNS = 'N', hemWE = 'E';
+		double lat = skymap.getdLatitude(), lon = skymap.getdLongitude();
+
+		if (lat < 0.0)
+			hemNS = 'S';
+		if (lon < 0.0)
+			hemWE = 'W';
+		
+		coordinate.setText(decimalToDMS(Math.abs(lat)) + " " + hemNS + ", " + decimalToDMS(Math.abs(lon)) + " " + hemWE);
+
+		skymap.updateSkyMap();	
+	}
+	
+	
+	/**
+	 * Convert decimal coordinates to DMS
+	 * 
+	 * From: https://en.wikipedia.org/wiki/Geographic_coordinate_conversion#Java_Implementation
+	 *
+	 * @param coord The decimal coordinate
+	 * @return A string in DMS form
+	 */
+	public static String decimalToDMS(double coord)
+	{
+		String output, degrees, minutes, seconds;
+
+		// gets the modulus the coordinate divided by one (MOD1).
+		// in other words gets all the numbers after the decimal point.
+		// e.g. mod = 87.728056 % 1 == 0.728056
+		//
+		// next get the integer part of the coord. On other words the whole number part.
+		// e.g. intPart = 87
+
+		double mod = coord % 1;
+		int intPart = (int) coord;
+
+		// set degrees to the value of intPart
+		// e.g. degrees = "87"
+
+		degrees = String.valueOf(intPart);
+
+		// next times the MOD1 of degrees by 60 so we can find the integer part for minutes.
+		// get the MOD1 of the new coord to find the numbers after the decimal point.
+		// e.g. coord = 0.728056 * 60 == 43.68336
+		// mod = 43.68336 % 1 == 0.68336
+		//
+		// next get the value of the integer part of the coord.
+		// e.g. intPart = 43
+
+		coord = mod * 60;
+		mod = coord % 1;
+		intPart = (int) coord;
+
+		// set minutes to the value of intPart.
+		// e.g. minutes = "43"
+		minutes = String.valueOf(intPart);
+
+		// do the same again for minutes
+		// e.g. coord = 0.68336 * 60 == 41.0016
+		// e.g. intPart = 41
+		coord = mod * 60;
+		intPart = (int) coord;
+
+		// set seconds to the value of intPart.
+		// e.g. seconds = "41"
+		seconds = String.valueOf(intPart);
+
+		// I used this format for android but you can change it
+		// to return in whatever format you like
+		// e.g. output = "87/1,43/1,41/1"
+		output = degrees + "° " + minutes + "' " + seconds + "''";
+
+		// Standard output of D°M′S″
+		// output = degrees + "°" + minutes + "'" + seconds + "\"";
+
+		return output;
 	}
 	
 	/**
